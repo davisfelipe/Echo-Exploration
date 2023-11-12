e7ce60aa4214b33445586766941adf2671d391c6
David Peña <dfph1996@gmail.com>
2023-11-11 20:06:39 -0500

    Feature: Update struct as response

--- handler/greetings.go
+++ handler/greetings.go
@@ -7,11 +7,19 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type HelloResponse struct {
+	Message string `json:"message"`
+	Success bool   `json:"success"`
+}
+
 func Hello(c echo.Context) error {
 	fmt.Println("Hola mundo")
 	qParams := c.Param("q")
 	if qParams == "" {
-		return c.JSON(http.StatusBadRequest, map[string]string{"message": "hola"})
+		return c.JSON(http.StatusBadRequest, HelloResponse{
+			Message: "Hola Mundo",
+			Success: true,
+		})
 	}
 	return c.JSON(http.StatusOK, map[string]string{"message": "hi"})
 }
